cd1808f9ea16928f01f4f0bdbce00553c83586ed
wrixcode <wrixcode@gmail.com>
2024-11-15 08:45:55 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Navbar/page'
+import Hero from './Hero/page'
+import About from './About/page'
+import Services from './Services/page'
+
+import Contact from './Contact/page'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Services/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default page
